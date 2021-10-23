4f3dd6423f58ec659e02c60f3f4139201656d25d
MatNimv <matilda.nimvik@gmail.com>
2021-10-23 16:22:21 +0200

    uhjja
    css främst :(

--- includes/header.php
+++ includes/header.php
@@ -13,28 +13,31 @@
 </head>
 <body>
     <main>
-    <h1>The Internet Dog Database</h1>
-    <div id="wrapper">
-        <?php
-        //är någon inloggad? no.
-            if (!isset($_SESSION["isLoggedIn"])){
-                echo "
+        <header>
+                <h1>The Internet Dog Database</h1>
+        </header>
+
+        <div id="wrapper">
+            <?php
+            //är någon inloggad? no.
+                if (!isset($_SESSION["isLoggedIn"])){
+                    echo "
+                        <nav>
+                            <h2><a href='/index.php'>Home</a></h2>
+                            <h2><a href='/list.php'>Dogs</a></h2>
+                            <h2><a href='/sign-in.php'>Sign In</a></h2>
+                        </nav>
+                    ";
+                } else {
+                    //är någon inloggad? yes.
+                    echo "
                     <nav>
                         <h2><a href='/index.php'>Home</a></h2>
                         <h2><a href='/list.php'>Dogs</a></h2>
-                        <h2><a href='/sign-in.php'>Sign In</a></h2>
+                        <h2><a href='/add.php'>Add</a></h2>
+                        <h2><a href='/profile.php'>Profile</a></h2>
+                        <h2><a href='/sign-out.php'>Sign Out</a></h2>
                     </nav>
                 ";
-            } else {
-                //är någon inloggad? yes.
-                echo "
-                <nav>
-                    <h2><a href='/index.php'>Home</a></h2>
-                    <h2><a href='/list.php'>Dogs</a></h2>
-                    <h2><a href='/add.php'>Add</a></h2>
-                    <h2><a href='/profile.php'>Profile</a></h2>
-                    <h2><a href='/sign-out.php'>Sign Out</a></h2>
-                </nav>
-            ";
-            }
-        ?>
+                }
+            ?>
